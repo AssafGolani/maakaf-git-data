a1e116901d299543f014a28b900bb21941a815a2
Assaf <1214assaf@gmail.com>
2024-03-14 10:33:45 +0200

    display all the repositories in the searchable scrollable dropdown list

--- src/components/ScrollableDropDown.jsx
+++ src/components/ScrollableDropDown.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import AsyncSelect from "react-select/async";
 
 export default function ScrollableDropDown({ repositories, loadingStatus }) {
+  const [reposName, setReposName] = useState([]);
   const filterOptions = (inputValue) => {
-    const repos = handleExtractNameOfOptions();
+    const repos = reposName;
     const selectedOption = repos.filter((i) =>
       i.label.toLowerCase().includes(inputValue.toLowerCase())
     );
     return selectedOption;
   };
+
+  useEffect(() => {
+    let repositoriesArray = [];
+    if (repositories && repositories.length > 0) {
+      repositoriesArray = repositories.map((repository) => {
+        return { value: repository.name, label: repository.name };
+      });
+      console.log(
+        "ReposName: " +
+          JSON.stringify(repositoriesArray) +
+          " " +
+          repositoriesArray.length
+      );
+      setReposName(repositoriesArray);
+    }
+  }, [repositories]);
 
   const loadOptions = (inputValue, callback) => {
     setTimeout(() => {
@@ -15,17 +33,12 @@ export default function ScrollableDropDown({ repositories, loadingStatus }) {
     }, 1000);
   };
 
-  const handleExtractNameOfOptions = () => {
-    return repositories.forEeach((repository) =>
-      repository.map((repo) => repo.name)
-    );
-  };
-
   return (
     <AsyncSelect
       cacheOptions
       loadOptions={loadOptions}
-      defaultOptions
+      defaultOptions={reposName}
+      defaultValue={reposName[0]}
       loadingStatus={loadingStatus}
     />
   );
