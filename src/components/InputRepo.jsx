724d159be228022c316c848ab1b76f4e448ef18d
GuyPaikowsky <GuyPaikowsky@users.noreply.github.com>
2024-03-17 10:39:53 +0200

    Revert temporarily to text box for repos input due to dropdown text insert bug.

--- src/components/InputRepo.jsx
+++ src/components/InputRepo.jsx
@@ -1,24 +1,16 @@
-import react from "react";
-import { Button, Flex, TextField } from "@radix-ui/themes";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import ScrollableDropDown from "./ScrollableDropDown";
+import React from "react";
+import {Button, Flex, TextField} from "@radix-ui/themes";
+import {CodeIcon, GitHubLogoIcon} from "@radix-ui/react-icons";
 
-function InputRepo({
-  handleFetch,
-  handleFetchAll,
-  handleDownload,
-  repositories,
-  loadingStatus,
-}) {
-  const [owner, setOwner] = react.useState("hasadna");
-  const [repo, setRepo] = react.useState("open-bus-map-search");
-  const [reposName, setReposName] = react.useState([]);
+function InputRepo({handleFetch, handleFetchAll, handleDownload, loadingStatus}) {
+  const [owner, setOwner] = React.useState("hasadna");
+  const [repo, setRepo] = React.useState("open-bus-map-search");
 
-  //TODO: Refactor and consolidate...
+
   function handleSubmit(owner, repo) {
     handleFetch(owner, repo);
-    setRepo("");
-    setOwner("");
+    // setRepo("");
+    // setOwner("");
   }
 
   function handleSubmitFullFetch(owner, repo) {
@@ -27,66 +19,57 @@ function InputRepo({
     setOwner("");
   }
 
-  react.useEffect(() => {
-    let repositoriesArray = [];
-    if (repositories && repositories.length > 0) {
-      repositoriesArray = repositories.map((repository) => {
-        return { value: repository.name, label: repository.name };
-      });
-      console.log(
-        "ReposName: " +
-          JSON.stringify(repositoriesArray) +
-          " " +
-          repositoriesArray.length,
-      );
-      setReposName(repositoriesArray);
-    }
-  }, [repositories]);
+  return (<Flex gap="4" justify="center">
+    <TextField.Root>
+      <TextField.Slot>
+        <GitHubLogoIcon height="16" width="16"/>
+      </TextField.Slot>
+      <TextField.Input
+        placeholder="Enter Owner…"
+        value={owner}
+        onChange={(e) => setOwner(e.target.value)}
+      />
+    </TextField.Root>
 
-  return (
-    <Flex gap="4" justify="center" wrap="wrap">
-      <TextField.Root>
-        <TextField.Slot>
-          <GitHubLogoIcon height="16" width="16" />
-        </TextField.Slot>
-        <TextField.Input
-          placeholder="Enter Owner…"
-          value={owner}
-          onChange={(e) => setOwner(e.target.value)}
-        />
-      </TextField.Root>
+    <TextField.Root>
+      <TextField.Slot>
+        <CodeIcon height="16" width="16"/>
+      </TextField.Slot>
+      <TextField.Input
+        placeholder="Enter Repo…"
+        value={repo}
+        onChange={(e) => setRepo(e.target.value)}
+      />
+    </TextField.Root>
 
-      <ScrollableDropDown options={reposName} loadingStatus={loadingStatus} />
+    <Button
+      size="2"
+      variant="soft"
+      onClick={() => handleSubmit(owner, repo)}
+      disabled={loadingStatus === true}
+    >
+      {loadingStatus === true ? "Loading..." : "Fetch Data Sample"}
+    </Button>
 
-      <Button
-        size="2"
-        variant="soft"
-        onClick={() => handleSubmit(owner, repo)}
-        disabled={loadingStatus === true}
-      >
-        {loadingStatus === true ? "Loading..." : "Fetch Data Sample"}
-      </Button>
+    <Button
+      size="2"
+      variant="soft"
+      onClick={() => handleSubmitFullFetch(owner, repo)}
+      disabled={loadingStatus === true}
+    >
+      {loadingStatus === true ? "Loading..." : "Fetch Full Data"}
+    </Button>
 
-      <Button
-        size="2"
-        variant="soft"
-        onClick={() => handleSubmitFullFetch(owner, repo)}
-        disabled={loadingStatus === true}
-      >
-        {loadingStatus === true ? "Loading..." : "Fetch Full Data"}
-      </Button>
-
-      <Button
-        size="2"
-        variant="soft"
-        color="crimson"
-        disabled={loadingStatus === true}
-        onClick={() => handleDownload()}
-      >
-        {loadingStatus === true ? "Loading..." : "Download CSV"}
-      </Button>
-    </Flex>
-  );
+    <Button
+      size="2"
+      variant="soft"
+      color="crimson"
+      disabled={loadingStatus === true}
+      onClick={() => handleDownload()}
+    >
+      {loadingStatus === true ? "Loading..." : "Download CSV"}
+    </Button>
+  </Flex>);
 }
 
 export default InputRepo;
